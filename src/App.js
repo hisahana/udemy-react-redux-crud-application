258ab003c7aee778946c4caf56308d21121b7118
華山 悠 <hanayama_hisashi@tenda.co.jp>
2020-09-02 14:21:51 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => console.log('I am clicked')} />
-    </React.Fragment>
-  )
+// Class Component
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+      </React.Fragment>
+    )
+  }
 }
 
+// Functional Component
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={() => console.log('I am clicked')} />
+//     </React.Fragment>
+//   )
+// }
+
+const Cat = () => {
+  return <div>Meow!</div>;
+}
 
 export default App;
